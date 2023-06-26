6ea7869d7ec74099b8248c93546e75b021817bb7
dennyrianto <137791089+dennyrianto@users.noreply.github.com>
2023-06-26 19:40:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function menuKem() {
+    var div = document.getElementById("menuNav2");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+      div.addEventListener("mouseleave", function() {
+        div.style.display = "none";
+      });
+    } else {
+      div.style.display = "none";
+    }
+  }
+
+function menuBer() {
+    var div = document.getElementById("menuNav3");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+      div.addEventListener("mouseleave", function() {
+        div.style.display = "none";
+      });
+    } else {
+      div.style.display = "none";
+    }
+  }
+
+  function menuTen() {
+    var div = document.getElementById("menuNav4");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+      div.addEventListener("mouseleave", function() {
+        div.style.display = "none";
+      });
+    } else {
+      div.style.display = "none";
+    }
+  }
+
+  function menuPend() {
+    var div = document.getElementById("menuNav5");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+      div.addEventListener("mouseleave", function() {
+        div.style.display = "none";
+      });
+    } else {
+      div.style.display = "none";
+    }
+  }
+
+  function menuPene() {
+    var div = document.getElementById("menuNav6");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+      div.addEventListener("mouseleave", function() {
+        div.style.display = "none";
+      });
+    } else {
+      div.style.display = "none";
+    }
+  }
